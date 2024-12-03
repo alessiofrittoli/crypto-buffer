a93f397f8b9332f0239ac7e261f8b596207200e0
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-03 19:48:30 +0100

    fix: add return Type to conversion functions

--- src/conversion.ts
+++ src/conversion.ts
@@ -4,7 +4,7 @@
  * @param string The string to convert.
  * @returns An Array of bytes.
  */
-export const stringToBytes = ( string: string ) => (
+export const stringToBytes = ( string: string ): number[] => (
 	[ ...stringToBuffer( string ) ]
 )
 
@@ -15,8 +15,8 @@ export const stringToBytes = ( string: string ) => (
  * @param string The string to convert.
  * @returns A Uint8Array or Node.js Buffer.
  */
-export const stringToBuffer = ( string: string ) => (
-	typeof window !== 'undefined'
-		? new Uint8Array( new TextEncoder().encode( string ) )
-		: Buffer.from( string )
+export const stringToBuffer = ( string: string ): Uint8Array | Buffer => (
+	typeof Buffer !== 'undefined'
+		? Buffer.from( string )
+		: new Uint8Array( new TextEncoder().encode( string ) )
 )
